24deb2fb148386a51b1d5b1e423e8e0ba03aee41
Yushin-Tati <y.tati8653@gmail.com>
2021-07-21 14:55:40 +0000

    検索機能導入開始、Gem'ransack'追加

--- Gemfile
+++ Gemfile
@@ -78,6 +78,7 @@ gem 'font-awesome-sass', '~> 5.13'
 gem 'dotenv-rails'
 gem 'bootstrap-honoka-rails', '~> 4.4.1'
 gem 'devise-i18n'
+gem 'ransack'
 
 group :production do
   gem 'mysql2'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  before_action :set_search
+  
+  def set_search
+    @q = Plan.ransack(params[:q])
+    @plans = @q.result(distinct: true).page(params[:page])
+  end
 
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
@@ -15,4 +22,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :telephone_number, :introduction])
   end
+  
+  private
+  
+  def search_params
+    params.require(:q).permit!
+  end
+  
 end
