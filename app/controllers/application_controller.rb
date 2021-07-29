78e0914f23a9f1fca94261a7300ab54d08157e78
Yushin-Tati <y.tati8653@gmail.com>
2021-07-29 13:57:24 +0000

    検索窓のレイアウト調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  
+
+
 
   def after_sign_in_path_for(resource)
     user_path(current_user.id)
@@ -17,11 +17,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :telephone_number, :introduction])
   end
-  
+
   private
-  
+
   def search_params
     params.require(:q).permit!
   end
-  
+
 end
