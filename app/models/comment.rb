5dbd0152aa5883635a91b5d879e57c2f03e284a5
Yushin-Tati <y.tati8653@gmail.com>
2021-08-05 13:52:45 +0000

    commentモデルにバリデーション追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :target, polymorphic: true, dependent: :destroy
   has_many :comments, as: :target
+  
+  with_options presence: true do
+    validates :
+  end
+  
 end
