f08bea99a1413aee815c9f61f1f1e802674b9af7
Yushin-Tati <y.tati8653@gmail.com>
2021-09-02 14:59:04 +0000

    [fixed]いいね機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,29 +1,29 @@
 class LikesController < ApplicationController
-  
+
   def create
     @like = current_user.likes.new
     @like.favorite_id = params[:favorite_id]
     @like.favorite_type = params[:favorite_type]
     @like.save
     if @like.favorite_type == "Plan"
-      redirect_to plans_path
+      redirect_to plan_path(params[:favorite_id])
     elsif @like.favorite_type == "Ability"
-      redirect_to abilities_path
+      redirect_to ability_path(params[:favorite_id])
     elsif @like.favorite_type == "Product"
-      redirect_to products_path
+      redirect_to product_path(params[:favorite_id])
     end
   end
-  
+
   def destroy
     like = current_user.likes.find_by(favorite_id: params[:favorite_id], favorite_type: params[:favorite_type])
     like.destroy
     if params[:favorite_type] == "Plan"
-      redirect_to plans_path
+      redirect_to plan_path(params[:favorite_id])
     elsif params[:favorite_type] == "Ability"
-      redirect_to abilities_path
+      redirect_to ability_path(params[:favorite_id])
     elsif params[:favorite_type] == "Product"
-      redirect_to products_path
+      redirect_to product_path(params[:favorite_id])
     end
   end
-  
+
 end
