4b84f5b04aecdce8ef83024573d44f79e8942f48
Yushin-Tati <y.tati8653@gmail.com>
2021-10-02 10:07:11 +0000

    [管理者]comment一覧ページ作成

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,14 +1,35 @@
 class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_search
+  
   def index
+    @comments = Comment.all
   end
 
   def show
-  end
-
-  def edit
+    @comment = Comment.find(params[:id])
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    if @comment.is_active == false
+      @comment.is_active = true
+    else
+      @comment.is_active = false
+    end
+    @comment.update(plan_params)
+    flash[:notice] = "ステータスを更新しました。"
+    redirect_to admin_comment_path(@comment.id)
+  end
+  
+  private
+  
+  def set_search
+    @q = Comment.ransack(params[:q])
+    @q_comments = @q.result(distinct: true).page(params[:page])
+  end
+  
+  def comment_params
+    params.permit(:is_active)
   end
 end
